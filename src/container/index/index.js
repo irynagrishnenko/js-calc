df8e5c1a9bf265acb22f6798b9e46b9accb510f3
irynagrishnenko <iryna.grishnenko@gmail.com>
2023-10-06 21:55:08 +0300

    calc

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,6 @@
-import '../../script/test'
+import { message, message2 } from '../../script/test'
+
+import MyName from '../../script/test'
 
 console.log('container')
+console.log(MyName)
--- src/script/test.js
+++ src/script/test.js
@@ -1,1 +1,11 @@
-console.log('script')
+// console.log('script')
+
+export const message = 'Default text'
+
+export const message2 = () => {}
+
+export function Message3() {}
+
+export const message3 = 'script'
+
+export default message
